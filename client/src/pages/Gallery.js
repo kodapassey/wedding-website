02180b94a91248bfd747d2c9db2cc969958267b2
Jonsey <mwjones2186@gmail.com>
2022-11-07 13:48:10 -0800

    package page created and containers created. added disclosures as well. Still some styling things to work out but on a page.

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,30 +1,25 @@
-import { Container, Card, Col, Button } from "react-bootstrap";
+import { useEffect } from "react";
 
-import img1 from "../components/images/Screenshot_20221102_053356.png" 
+import { Container, Card, Col, Button, Row } from "react-bootstrap";
+// import { images } from "../components/GalleryImages";
+import MyGallery from "../components/GalleryImages"
+
 
 export default function Gallery() {
-  return (
-    <>
-      <div>
-        <Container>
-          <h1>Gallery</h1>
-          <p>Check out the pics below</p>
-        </Container>
-      </div>
-      <div className="App">
-        <Container className="p-4">
-          <Col md="4">
-            <Card>
-              <Card.Img variant="top" src={img1} />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-              
-                {/* <Button variant="primary">Read More</Button> */}
-              </Card.Body>
-            </Card>
-          </Col>
-        </Container>
-      </div>
-    </>
-  );
+    return (
+        <>
+        <div>
+            <Container>
+            <h1>Gallery</h1> 
+                <p>
+                    Take a look at some of our events
+                </p>
+            </Container>
+        </div>
+        <div>
+            <MyGallery />
+        </div>
+       
+       </>
+    )
 }
